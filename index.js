53cf4e43abc5e0a9ce536e60996a1c45b4b8e485
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-03-16 21:02:44 +0700

    Add env file

--- index.js
+++ index.js
@@ -1,80 +1,15 @@
+require('dotenv').config();
 const express = require("express");
-const PORT = 2000;
 const app = express();
 
 const bodyParser=require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-let products=[
-    {
-        id:1,
-        name: "Nasi Goreng",
-        price: 15000
-    },
-    {
-        id:2,
-        name: "Soto",
-        price: 20000
-    }
-]
+const routes = require('./routes');
 
-app.get("/api", (req,res)=>{
-    res.json({
-        status: 'ok',
-        data: products
-    })
-});
-app.post("/api", (req,res)=>{
-    const newProduct={
-        id:3,
-        name: "Pempek",
-        price: 50000
-    }
-    products.push(newProduct)
-    res.json({
-        status:'ok',
-        message:'new product has successfully created',
-        data: products
-    })
-});
-app.put("/api", (req,res)=>{
-    const newProduct={
-        name: "Pempek",
-        price: 50000
-    }
-    const index=products.findIndex(v=> v.id===2);
+app.use('/api', routes)
 
-    products[index]=newProduct;
-
-    res.json({
-        status:'ok',
-        message:'data successfully replaced',
-        data: products
-    })
-});
-app.patch("/api", (req,res)=>{
-    const index=products.findIndex(v=> v.id===2);
-
-    products[index]["price"]=99999;
-    res.json({
-        status:'ok',
-        message:'data successfully updated',
-        data: products
-    })
-});
-app.delete("/api", (req,res)=>{
-    const indexSoto=products.findIndex(v=> v.name==="Soto");
-
-    products.splice(indexSoto,1)
-
-    res.json({
-        status:'ok',
-        message:'data successfully deleted',
-        data: products
-    })
-});
-
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log(`server running on port ${PORT}....`);
 });
